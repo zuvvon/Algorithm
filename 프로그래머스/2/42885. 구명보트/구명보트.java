c2cecfcd59eff157dc183224381b04c4e266ec9e
Juwon <juwon0404@nate.com>
2025-07-01 01:54:20 +0900

    [level 2] Title: 구명보트, Time: 12.39 ms, Memory: 58.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -0,0 +1,23 @@
+
+import java.util.Arrays;
+
+public class Solution {
+    public int solution(int[] people, int limit) {
+        Arrays.sort(people);
+        
+        int left = 0;                   
+        int right = people.length - 1;  
+        int boats = 0;               
+        
+        while (left <= right) {
+            if (people[left] + people[right] <= limit) {
+                left++;
+                right--;
+            } else {
+                right--; 
+            }
+            boats++;
+        }  
+        return boats;
+    }
+}
