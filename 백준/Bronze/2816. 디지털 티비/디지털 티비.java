753d9714d7ace540c865f63459dc5aacc4aebdaa
Juwon <juwon0404@nate.com>
2025-10-04 19:09:42 +0900

    [Bronze I] Title: 디지털 티비, Time: 188 ms, Memory: 17828 KB -BaekjoonHub

--- 백준/Bronze/2816. 디지털 티비/디지털 티비.java
+++ 백준/Bronze/2816. 디지털 티비/디지털 티비.java
@@ -1,50 +1,47 @@
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        String[] arr = new String[n];
-        
-        for(int i = 0; i < n; i++) {
-            arr[i] = sc.next();
-        }
-        
-        // 1단계: KBS1을 첫 번째로 이동
-        for(int i = 0; i < n; i++) {
-            if(arr[i].equals("KBS1")) { // KBS1을 찾았으면
-                String tmp = "";
-                for(int j = i; j > 0; j--) {
-                    System.out.print(4);
-                    tmp = arr[j-1];
-                    arr[j-1] = arr[j];
-                    arr[j] = tmp;
-                }
-                break;
-            } else {
-                System.out.print(1); // KBS1을 찾을 때까지 화살표 이동
+        sc.nextLine();
+        int pos1 = -1;
+        int pos2 = -1;
+
+        // 위치 발견
+        for (int i = 0; i < n; i++) {
+            String channel = sc.nextLine();
+            if (channel.equals("KBS1")){
+                pos1 = i;
+            }
+            if (channel.equals("KBS2")){
+                pos2 = i;
             }
         }
-        
-        // 중간 체크: KBS2가 이미 두 번째에 있으면 종료
-        if(arr[1].equals("KBS2")) {
-            return;
+
+        StringBuilder result = new StringBuilder();
+
+        // KBS1 올리기
+        for (int i = 0; i < pos1; i++) {
+            result.append("1");
         }
-        
-        // 2단계: KBS2를 두 번째로 이동
-        for(int i = 0; i < n; i++) {
-            if(arr[i].equals("KBS2")) { // KBS2를 찾았으면
-                String tmp = "";
-                for(int j = i; j > 1; j--) { // j > 1: 두 번째까지만
-                    System.out.print(4);
-                    tmp = arr[j-1];
-                    arr[j-1] = arr[j];
-                    arr[j] = tmp;
-                }
-                break;
-            } else {
-                System.out.print(1); // KBS2를 찾을 때까지 화살표 이동
-            }
+        for (int i = 0; i < pos1; i++) {
+            result.append("4");
         }
+
+        // KBS2 위치 조정
+        if (pos2 < pos1){
+            pos2++;
+        }
+
+        // KBS2 올리기
+        for (int i = 0; i < pos2; i++) {
+            result.append("1");
+        }
+        for (int i = 0; i < pos2 - 1; i++) {
+            result.append("4");
+        }
+
+        System.out.println(result);
     }
 }
