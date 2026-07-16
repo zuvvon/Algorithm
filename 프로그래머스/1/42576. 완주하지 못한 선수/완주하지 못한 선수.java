c907c9eeafc3b8e82d6619f18fdaa8a87a3d27f6
Juwon <juwon0404@nate.com>
2026-07-16 14:42:36 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 56.40 ms, Memory: 115 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -2,15 +2,26 @@ import java.util.*;
 
 class Solution {
     public String solution(String[] participant, String[] completion) {
-        Arrays.sort(participant);
-        Arrays.sort(completion);
-        
-        // 중간에 하나 틀리면 그게 답임
-        for(int i =0; i < completion.length; i++){
-            if(!participant[i].equals(completion[i]))
-                return participant[i];
+
+        HashMap<String, Integer> map = new HashMap<>();
+
+        // 참가자 수 저장
+        for (String name : participant) {
+            map.put(name, map.getOrDefault(name, 0) + 1);
         }
-        // 만약 끝까지 같으면 마지막이 답
-        return participant[participant.length -1];
+
+        // 완주한 사람 수 차감
+        for (String name : completion) {
+            map.put(name, map.get(name) - 1);
+        }
+
+        // 값이 1 이상인 사람이 완주하지 못한 사람
+        for (String key : map.keySet()) {
+            if (map.get(key) > 0) {
+                return key;
+            }
+        }
+
+        return "";
     }
 }
