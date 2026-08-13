60e25c85df0d3c9e5e23c6c126a29321832f5280
Juwon <juwon0404@nate.com>
2026-08-13 16:39:47 +0900

    [level 2] Title: 의상, Time: 0.41 ms, Memory: 73.3 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -2,19 +2,16 @@ import java.util.*;
 
 class Solution {
     public int solution(String[][] clothes) {
-        // (각 종류별 개수 +1하고 서로 곱하기)-1
+        // (각 종류별 개수 +1하고 서로 곱하기)-1;
         HashMap<String, Integer> map = new HashMap<>();
-        
         for(String[] cloth: clothes){
             String type = cloth[1];
-            map.put(type, map.getOrDefault(type,0)+1);
+            map.put(type, map.getOrDefault(type, 0) +1);
         }
-        
         int cnt = 1;
-        
         for(Integer value: map.values()){
             cnt *= (value +1);
         }
-        return cnt - 1;
+        return cnt -1;
     }
 }
