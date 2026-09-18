896936384a18eeb27275c3ccd5166bcd8b027a47
Juwon <juwon0404@nate.com>
2026-09-18 14:27:11 +0900

    [level 1] Title: 폰켓몬, Time: 2.25 ms, Memory: 87.7 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -2,10 +2,12 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] nums) {
+        // 종류는 3, 배열/2는 2, 둘다 구해서 더 작은 값을 리턴하도록
+        
         HashSet<Integer> set = new HashSet<>();
-        for(Integer num: nums){
+        for(int num: nums){
             set.add(num);
         }
-        return Math.min(set.size(), nums.length/2);
+        return Math.min(nums.length / 2, set.size());
     }
 }
